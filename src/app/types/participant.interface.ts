7f03190f17ac2de26906395fd6ae5a759b2e9a99
Christophe Porte <porte17christophe69@gmail.com>
2026-08-20 11:57:53 +0200

    feat/readParticipantList: définition de l'interface.

--- src/app/types/participant.interface.ts
+++ src/app/types/participant.interface.ts
@@ -1,1 +1,7 @@
-export interface ParticipantInterface {}
+export interface ParticipantInterface {
+	firstName: string;
+	lastName: string;
+	email: string;
+	status: string;
+	ticketType: string;
+}
