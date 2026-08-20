58c979ad0efa0ef60bf9d40ffd9efc0e4b01408a
mamadoulamarana1732-hue <mamadoulamarana1732@gmail.com>
2026-08-20 14:03:39 +0200

    Création du composant ticket card

---
+++ src/app/components/ticket-card.component/ticket-card.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-ticket-card.component',
+  styleUrl: './ticket-card.component.css',
+  templateUrl: './ticket-card.component.html',
+})
+export class TicketCardComponent {}
