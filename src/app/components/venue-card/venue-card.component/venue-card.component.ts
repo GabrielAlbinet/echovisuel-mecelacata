324940aca8f5b110f2434292ddfc2894cdb013b4
Martin Mabrut <martinmabrut43@gmail.com>
2026-08-20 11:46:04 +0200

    Création du venue-card.component

---
+++ src/app/components/venue-card/venue-card.component/venue-card.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-venue-card.component',
+  styleUrl: './venue-card.component.css',
+  templateUrl: './venue-card.component.html',
+})
+export class VenueCardComponent {}
