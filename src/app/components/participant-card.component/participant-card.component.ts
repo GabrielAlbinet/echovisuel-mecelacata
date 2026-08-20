6c8fb90e2bd52ada12cee42bffec401c6b2d2d86
Christophe Porte <porte17christophe69@gmail.com>
2026-08-20 11:44:34 +0200

    feat/readParticipantList: ajout des fichiers de composants partcipant-card et participant-list, de l'interface participant, et du service participant.

---
+++ src/app/components/participant-card.component/participant-card.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-participant-card.component',
+  styleUrl: './participant-card.component.css',
+  templateUrl: './participant-card.component.html',
+})
+export class ParticipantCardComponent {}
---
+++ src/app/components/participant-list.component/participant-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-participant-list.component',
+  styleUrl: './participant-list.component.css',
+  templateUrl: './participant-list.component.html',
+})
+export class ParticipantListComponent {}
