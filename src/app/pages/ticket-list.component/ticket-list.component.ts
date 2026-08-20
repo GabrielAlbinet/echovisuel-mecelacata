716f9ea0bf852383ddec2488688b33c1806d3433
mamadoulamarana1732-hue <mamadoulamarana1732@gmail.com>
2026-08-20 14:05:42 +0200

    Création du composant ticket-list

---
+++ src/app/pages/ticket-list.component/ticket-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-ticket-list.component',
+  styleUrl: './ticket-list.component.css',
+  templateUrl: './ticket-list.component.html',
+})
+export class TicketListComponent {}
