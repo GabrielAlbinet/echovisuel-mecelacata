d28b4cc0aa50a0b6ff59ff2a091d077851e15742
Martin Mabrut <martinmabrut43@gmail.com>
2026-08-20 11:42:02 +0200

    Création du venue-list.component

---
+++ src/app/components/venue-list/venue-list.component/venue-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-venue-list.component',
+  styleUrl: './venue-list.component.css',
+  templateUrl: './venue-list.component.html',
+})
+export class VenueListComponent {}
