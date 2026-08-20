10025a8926cb59c35fb388ce9ad153b2038df67a
mamadoulamarana1732-hue <mamadoulamarana1732@gmail.com>
2026-08-20 14:32:03 +0200

    Appel du composant ticket card

--- src/app/pages/ticket-list.component/ticket-list.component.ts
+++ src/app/pages/ticket-list.component/ticket-list.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Ticket } from '../../types/ticket-type.interface';
+import { TicketService } from '../../services/ticket.service';
+import { TicketCardComponent } from '../../components/ticket-card.component/ticket-card.component';
 
 @Component({
-  imports: [],
+  imports: [TicketCardComponent],
   selector: 'app-ticket-list.component',
   styleUrl: './ticket-list.component.css',
   templateUrl: './ticket-list.component.html',
 })
-export class TicketListComponent {}
+export class TicketListComponent {
+  private readonly ticketService = inject(TicketService);
+  readonly ticketList = this.ticketService.ticketListSignal; //Creation d'un signal d'une liste de ticket
+  
+  ngOnInit(){
+    this.ticketService.getTicketList();
+  }
+}
