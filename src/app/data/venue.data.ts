750f7c739fb4b7124ea63cd674260c9c3b6d9d54
Martin Mabrut <martinmabrut43@gmail.com>
2026-08-20 15:35:33 +0200

    Affichage d'un message lors de l'absence de lieu dans la data

--- src/app/data/venue.data.ts
+++ src/app/data/venue.data.ts
@@ -28,22 +28,22 @@ export const data: Venue[] = [
     {
         name: "Coopérative de mai",
         type: "Salle de Concert",
-        capacity: 1000,
+        capacity: 500,
         location: "Clermont-Ferrand",
         image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNO0MP7JQgnVqb074Ps2rpJ_p6miOJECk1qzJ8Gk4JGA&s=10"
     },
     {
        name: "Transbordeur",
         type: "Salle de Concert",
-        capacity: 1000,
+        capacity: 500,
         location: "Lyon",
         image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSM_Wf26a1Q313DOQnRQEwKq1q4yNwod-ayND_CL5hPIg&s=10" 
     },
     {
         name: "Le Fil",
         type: "Salle de Concert",
-        capacity: 1000,
+        capacity: 500,
         location: "Saint-Etienne",
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbTluNIbPmNWniRTAEikAwfFSk5EqSI0P7eRbNU-DFqg&s=10"
+        image: ""
     }
 ]
