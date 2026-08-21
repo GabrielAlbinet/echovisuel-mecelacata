b700569fd292ebd6f7a958751ef158954576fc77
mamadoulamarana1732-hue <mamadoulamarana1732@gmail.com>
2026-08-21 11:54:43 +0200

    creation formulaire de creation de billet avec validator

--- src/app/components/create-ticket.component/create-ticket.component.ts
+++ src/app/components/create-ticket.component/create-ticket.component.ts
@@ -2,6 +2,7 @@ import { Component, output } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Ticket } from '../../types/ticket-type.interface';
 
+
 @Component({
   imports: [ReactiveFormsModule],
   selector: 'app-create-ticket',
@@ -18,34 +19,5 @@ export class CreateTicketComponent {
     dateBeginning : new FormControl(new Date(), Validators.required),
     dateEnd : new FormControl(new Date(), Validators.required),
   });
-  submitTicket(){
-    if(this.createTicketForm.controls.name.value != null &&
-      this.createTicketForm.controls.description.value != null &&
-      this.createTicketForm.controls.price.value != null &&
-      this.createTicketForm.controls.quantity.value != null &&
-      this.createTicketForm.controls.dateBeginning.value != null &&
-      this.createTicketForm.controls.dateEnd.value != null &&
-      this.createTicketForm.controls.dateBeginning.value < this.createTicketForm.controls.dateEnd.value
-    ){
-      if(this.createTicketForm.controls.quantity.value == 0){
-        this.createdticket.emit({
-        name : this.createTicketForm.controls.name.value,
-        description : this.createTicketForm.controls.description.value,
-        price : this.createTicketForm.controls.price.value,
-        quantity : this.createTicketForm.controls.quantity.value,
-        available : false,
-        condition : `Validité : ${this.createTicketForm.controls.dateBeginning.value} au ${this.createTicketForm.controls.dateEnd.value}`,
-      });
-      }else{
-        this.createdticket.emit({
-        name : this.createTicketForm.controls.name.value,
-        description : this.createTicketForm.controls.description.value,
-        price : this.createTicketForm.controls.price.value,
-        quantity : this.createTicketForm.controls.quantity.value,
-        available : true,
-        condition : `Validité : ${this.createTicketForm.controls.dateBeginning.value} au ${this.createTicketForm.controls.dateEnd.value}`,
-      });
-      }
-    }
-  }
+  submitTicket(){}
 }
