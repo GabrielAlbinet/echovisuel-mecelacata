687135c9f0627ca0668e58f2264f91de38264466
Gabriel Albinet <gabriel.albinet.pro@gmail.com>
2026-09-04 15:20:56 +0200

    rate limit introduit, test effectué avec une limite de 5 requests

--- api/app.ts
+++ api/app.ts
@@ -1,11 +1,15 @@
 import Express from "express";
+import rateLimit from "express-rate-limit";
 import artistRouter from "./src/routes/artist.router.ts";
 
 const express = Express;
 const app = express();
 const port = 3000;
 
+const limiter = rateLimit ({windowMs: 15 * 60 * 1000, limit: 5}); //5 reequêtes max par 15 mins
+
 app.use(express.json());
+app.use(limiter);
 app.use("/api", artistRouter); // connection de l'API au projet
 
 app.listen(port, () => {
