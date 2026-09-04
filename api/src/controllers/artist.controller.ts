db3dc51fc812d15c97c4021b9a427d0a74258154
Gabriel Albinet <gabriel.albinet.pro@gmail.com>
2026-09-04 11:15:28 +0200

    console.log de test d'endpoint retiré

--- api/src/controllers/artist.controller.ts
+++ api/src/controllers/artist.controller.ts
@@ -2,7 +2,6 @@ import artistService from "../services/artist.apiservice.ts";
 import type { ArtistDTO } from "../dto/artist.dto.ts";
 
 const getAll = async (req, res, next) => {
-  console.log(">>> getAll appelé");
   try {
     const artists: ArtistDTO[] = await artistService.getAll();
     res.status(200).json(artists);
