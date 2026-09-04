afd3b3436df2aa9f4757f64c6a56d1cd23828dcb
Gabriel Albinet <gabriel.albinet.pro@gmail.com>
2026-09-04 10:42:37 +0200

    middleware fait

---
+++ api/src/middlewares/artist.middleware.ts
@@ -0,0 +1,15 @@
+import artistService from "../services/artist.apiservice.ts";
+
+const checkExists = async (req, res, next) => {
+  try {
+    const artist = await artistService.getById(Number(req.params.id));
+    req.artist = artist;
+    next();
+  } catch (error) {
+    return res.status(404).json({
+      message: "Aie aie aie ma canaille, on a pas trouvé d'artiste...",
+    });
+  }
+};
+
+export default { checkExists };
