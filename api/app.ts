8ca0db2e8e036f6ad2eedfccca220f0fdd22f3c2
Gabriel Albinet <gabriel.albinet.pro@gmail.com>
2026-09-04 11:11:01 +0200

    1er endpoint fait, getAll testé

--- api/app.ts
+++ api/app.ts
@@ -1,12 +1,12 @@
 import Express from "express";
+import artistRouter from "./src/routes/artist.router.ts";
 
 const express = Express;
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(express.json());
+app.use("/api", artistRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
