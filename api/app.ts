7086b8a087b0b3935374e4043db96a524649d828
Gabriel Albinet <gabriel.albinet.pro@gmail.com>
2026-09-22 11:04:50 +0200

    validator de ticket remis, disparu au 1er commit des validators

--- api/app.ts
+++ api/app.ts
@@ -1,18 +1,27 @@
 import Express from "express";
 import rateLimit from "express-rate-limit";
-import artistRouter from "./src/routes/artist.router.ts";
 import cors from "cors";
+import artistRouter from "./src/routes/artist.router.ts";
+import venueRouter from "./src/routes/venue.router.ts";
+import ticketRouter from "./src/routes/ticket.router.ts";
 
 const express = Express;
 const app = express();
 const port = 3000;
 
-const limiter = rateLimit ({windowMs: 15 * 60 * 1000, limit: 5}); //5 reequêtes max par 15 mins
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 100,
+});
 
 app.use(express.json());
 app.use(limiter);
-app.use(cors({origin: "http://localhost:4200"}))
-app.use("/api", artistRouter); // connection de l'API au projet
+app.use(cors({
+  origin: "http://localhost:4200",
+}));
+app.use("/api", artistRouter);
+app.use("/api", venueRouter);
+app.use("/api", ticketRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
